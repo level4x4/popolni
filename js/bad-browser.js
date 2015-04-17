3ab0f5f52a631419c29cbfbcf27d5780c7afb11f
Aleksandr Nikitin <a.nikitin@i.ua>
2015-04-17 11:55:44 +0300

    Bad browser

---
+++ js/bad-browser.js
@@ -0,0 +1,46 @@
+/**
+ * Created by Shnur on 17.04.2015.
+ */
+(function(){
+	window.badBrowser = function(parametrs) {
+		var ua = navigator.userAgent, tem,
+			M = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [],
+			bb = {};
+		if (/trident/i.test(M[1])) {
+			tem = /\brv[ :]+(\d+)/g.exec(ua) || [];
+			return {
+				browser: 'ie',
+				version: tem[1] || ''
+			};
+		}
+		if (M[1] === 'Chrome') {
+			tem = ua.match(/\bOPR\/(\d+)/);
+			if (tem != null) {
+				return {
+					browser: 'opera',
+					version: tem[1]
+				};
+			}
+		}
+		M = M[2] ? [M[1], M[2]] : [navigator.appName, navigator.appVersion, '-?'];
+		if ((tem = ua.match(/version\/(\d+)/i)) != null) {
+			M.splice(1, 1, tem[1]);
+			bb = {
+				browser: M[0].toLowerCase(),
+				version: M[1]
+			}
+		}
+		bb = {
+			browser: M[0].toLowerCase(),
+			version: M[1]
+		};
+		for (var i in parametrs) {
+			if (parametrs[i].browser === bb.browser && bb.version <= parametrs[i].version) {
+				document.getElementsByTagName('html')[0].className += ' bad-browser';
+				document.body.innerHTML = html;
+			}
+		}
+	};
+
+	var html = '<div class="bb-browser"><div style="width: 700px; margin: 0 auto; text-align: left; padding: 0; overflow: hidden;"><div style="width: 490px;"><div class="bb-title">Вы используете устаревший браузер</div><div class="bb-text">В целях безопасности и для более удобной работы с сайтом обновите Ваш браузер на более современный.</div></div><ul class="bb-ul"><li><a href="http://windows.microsoft.com/ru-ru/internet-explorer/download-ie" target="_blank"><img src="images/browser/ie9-10_64x64.png" width="64" height="64" title="Internet Explorer" /></a></li><li><a href="http://www.midori-browser.org/download/" target="_blank"><img src="images/browser/midori_64x64.png" width="64" height="64" title="Midori" /></a></li><li><a href="http://browser.yandex.ru/" target="_blank"><img src="images/browser/yandex_64x64.png" width="64" height="64" title="Yandex Browser" /></a></li><li><a href="https://www.apple.com/ru/safari/" target="_blank"><img src="images/browser/safari_64x64.png" width="64" height="64" title="Safari" /></a></li><li><a href="http://www.opera.com/ru/" target="_blank"><img src="images/browser/opera_64x64.png" width="64" height="64" title="Opera" /></a></li><li><a href="http://www.google.com/intl/ru/chrome/" target="_blank"><img src="images/browser/chrome_64x64.png" width="64" height="64" title="Google Chrome" /></a></li><li><a href="http://www.mozilla.org/ru/firefox/fx/" target="_blank"><img src="images/browser/firefox_64x64.png" width="64" height="64" title="Mozilla Firefox" /></a></li><li><a href="http://ru.ucweb.com/ucbrowser/download/" target="_blank"><img src="images/browser/uc_browser_72x72.png" width="64" height="64" title="UC Browser" /></a></li></ul></div></div>';
+})();
--- js/common.js
+++ js/common.js
@@ -6,12 +6,28 @@
 	};
 
 	var classNames = {
+		pcOverflowHidden: 'pc_overflow_hidden',
+		overflowAuto: '_overflow_auto',
+		overflowYAuto: '_overflow-y_auto',
+		hide: 'pc-hide',
+
+		pcWrapper: 'pc-wrapper',
 		pcLanguages: 'pc-languages',
 		pcOpen: 'pc-open',
 		pcShowShortText: 'pc-show-short-text',
 		pcTextHeight: 'pc-text-height',
 		pcShowFullText: 'pc-show-full-text',
-		pcShowFullTextButton: 'pc-show-full-text-button'
+		pcShowFullTextButton: 'pc-show-full-text-button',
+
+		pcNav: 'pc-nav',
+		pcMobileMenu: 'pc-mobile-menu',
+		pcHeaderNav: 'pc-header-nav',
+		pcShowNav: 'pc-show-nav',
+		pcBackgroundMenu: 'pc-background-menu',
+		pcUlNavContainer: 'pc-ul-nav-container',
+		pcMobileScroll: 'pc-mobile-scroll',
+
+		badBrowser: 'bad-browser'
 	};
 
 	var ids = {};
@@ -35,23 +51,38 @@
 
 	var selectors = pc.buildSelectors(classNames, ids);
 
-	var $pcLanguages,
+	var $html,
+		$body,
+		$pcWrapper,
+		$pcMobileMenu,
+		$pcLanguages,
 		$pcTextHeight,
-		$pcShowShortText;
+		$pcShowShortText,
+		$pcBackgroundMenu,
+
+		isShowMenu = false;
 
 	pc.preloadImage = function(url){
 		var image = new Image();
 		image.src = url;
 	};
 
-	$(function(){
-		pc.preloadImage('/images/pc-slide-bg-1920.jpg');
-		pc.preloadImage('/images/pc-slide-bg-995.jpg');
-		pc.preloadImage('/images/pc-slide-bg-768.jpg');
+	pc.preloadImage('/images/pc-slide-bg-1920.jpg');
+	pc.preloadImage('/images/pc-slide-bg-995.jpg');
+	pc.preloadImage('/images/pc-slide-bg-768.jpg');
 
+	$(function(){
+		$html = $('html');
+		$body = $('body');
+
+		badBrowser([{browser: 'safari', version: '5'}]);
+
+		$pcWrapper = $(selectors.pcWrapper);
+		$pcMobileMenu = $(selectors.pcMobileMenu);
 		$pcLanguages = $(selectors.pcLanguages);
 		$pcShowShortText = $(selectors.pcShowShortText);
 		$pcTextHeight = $(selectors.pcTextHeight);
+		$pcBackgroundMenu = $(selectors.pcBackgroundMenu);
 
 		$pcLanguages.on('click', 'a:first', function(){
 			$pcLanguages.toggleClass(classNames.pcOpen);
@@ -70,6 +101,38 @@
 				} else {
 					$pcShowShortText.animate({height: 70}, 1000).removeClass(classNames.pcShowFullText);
 					$this.text(pc.lang.showAllText);
+				}
+			});
+		}
+
+		if ($pcMobileMenu.exists()) {
+			$pcMobileMenu.on('click', function(){
+				var $this = $(this);
+				var $pcHeaderNav = $this.closest(selectors.pcHeaderNav);
+				var $pcMobileScroll = $pcHeaderNav.find(selectors.pcMobileScroll);
+				var windowHeight = $pcMobileScroll.height();
+
+				if (!isShowMenu) {
+					//$body.addClass(classNames.pcOverflowHidden);
+					$pcHeaderNav.addClass(classNames.pcShowNav);
+					$pcBackgroundMenu.removeClass(classNames.hide);
+					$pcWrapper.css({height: (windowHeight + 82)}).addClass(classNames.pcOverflowHidden);
+					$pcMobileScroll.one('transitionend webkitTransitionEnd oTransitionEnd otransitionend MSTransitionEnd', function() {
+						if($html.hasClass('ios') || $html.hasClass('android')) {
+							return false;
+						}
+						$pcMobileScroll.perfectScrollbar({suppressScrollX: true});
+						$pcMobileScroll.perfectScrollbar('update');
+					});
+					isShowMenu = true;
+				} else {
+					$pcHeaderNav.removeClass(classNames.pcShowNav);
+					$pcMobileScroll.one('transitionend webkitTransitionEnd oTransitionEnd otransitionend MSTransitionEnd', function() {
+						//$body.removeClass(classNames.pcOverflowHidden);
+						$pcWrapper.css({height: '100%'}).removeClass(classNames.pcOverflowHidden);
+						$pcBackgroundMenu.addClass(classNames.hide);
+					});
+					isShowMenu = false;
 				}
 			});
 		}
